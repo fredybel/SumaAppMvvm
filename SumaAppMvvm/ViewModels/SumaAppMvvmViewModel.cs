cad712bb625959b1945c4e9af0164920829f0be8
fredybel <fredybeltran712@gmail.com>
2024-11-29 15:17:57 -0600

    update suma y botones
    viernes 3pm

--- SumaAppMvvm/ViewModels/SumaAppMvvmViewModel.cs
+++ SumaAppMvvm/ViewModels/SumaAppMvvmViewModel.cs
@@ -1,20 +1,52 @@
 ﻿
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SumaAppMvvm.ViewModels
 {
     public partial class SumaAppMvvmViewModel : ObservableObject
     {
         [ObservableProperty]
-        private double n1;
+        private double a;
 
         [ObservableProperty]
-        private double n2;
+        private double b;
 
+        [ObservableProperty]
+        private double suma;
 
         private void Alerta(string Titulo, string Mensaje)
         {
             MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
         }
+
+        [RelayCommand]
+        private void Calcular()
+        {
+            
+            try
+            {
+               
+                Suma = A + B;
+
+               
+            }
+
+
+            catch (Exception ex)
+            {
+                Alerta("ERROR", ex.Message);
+            }
+        }
+        [RelayCommand]
+        private void Limpiar()
+        {
+            A = 0;
+            B = 0;
+            Suma = 0;
+
+        }
     }
-}
+    }
+
+
